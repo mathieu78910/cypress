add2f7c1cf7caffb9a9d3f879ec99bfec9586c9f
mathieu7891O <mathieu.monnie7@gmail.com>
2026-04-21 14:12:36 +0200

    Correction du texte de bienvenue pour refléter le nom correct "CESI MA STAR".

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">CESI Ma STAR</a>
+          Welcome to <a href="https://nextjs.org">CESI MA STAR</a>
         </h1>
 
         <p className={styles.description}>
